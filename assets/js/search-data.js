018f18cda92988e0f4b19f6228beeb7508791409
patriciogoldstein <46763334+patriciogoldstein@users.noreply.github.com>
2026-05-06 03:42:02 +0000

    Deploying to gh-pages from @ patriciogoldstein/patriciogoldstein.github.io@03610fd8122fa1c40cdd67d271b66d577f609576 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,21 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "news-a-simple-inline-announcement",
+  },{id: "nav-repositories",
+          title: "repositories",
+          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/repositories/";
+          },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "Course materials, schedules, and resources for classes taught.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
